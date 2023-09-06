bbebea5d42797c197619697f5f42f0b125a89d68
Muhammed Jaseer <mjaseer43@gmail.com>
2023-09-06 12:09:18 +0530

    cors updated more

--- index.js
+++ index.js
@@ -27,29 +27,39 @@ app.use(morgan('dev'))
 db()
 
 const server = createServer(app);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://64f812281a439a3ba9b52ffe--projectonia.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Enable credentials
+
+    next();
+});
+
 const io = new Server(server, {
     pingTimeout: 60000,
     cors: {
         credentials: true,
-        origin:'*'
+        origin: process.env.allowedOrigins
     }
 });
 
 let activeUsers = {}
 socketConnect(io, activeUsers)
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://64f812281a439a3ba9b52ffe--projectonia.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 app.use(express.json())
 app.use(cors({
     credentials: true,
-    origin: '*',
-    methods:["GET,HEAD,OPTIONS,POST,PUT"]
+    origin: process.env.allowedOrigins,
+    methods: ["GET,HEAD,OPTIONS,POST,PUT"]
 }))
 
 app.use(bodyParser.json())
