0047d0e1e8e6de28ed8f782e13116c40f75a6d83
Muhammed Jaseer <mjaseer43@gmail.com>
2023-09-06 11:41:58 +0530

    cors updated again

--- index.js
+++ index.js
@@ -38,13 +38,10 @@ const io = new Server(server, {
 let activeUsers = {}
 socketConnect(io, activeUsers)
 
-app.use((req, res, next) => {
-    res.set({
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-    });
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
 });
 
