f7533c0857264d268f0bc7b97f76e267ead11f47
Muhammed Jaseer <mjaseer43@gmail.com>
2023-10-16 17:58:46 +0530

    mongo updated

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -4,6 +4,8 @@ function dbConnect() {
     mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
+    },()=>{
+        console.log('MongoDB connected');
     }).catch((err) => {
         console.log('data base error\n' + err);
     })
